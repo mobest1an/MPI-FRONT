7bbd9eeac94aee026ddf6931ce5b305e4b977ce9
ruaktiq <ivan.kustarev@raiffeisen.ru>
2025-12-22 21:45:05 +0300

    login default routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import Commissar from './pages/Commissar';
 import PrivateRoute from './components/PrivateRoute';
 import Escort from './pages/Escort';
+import { useAuth } from './context/AuthContext';
 
 function App() {
+    const { isAuthenticated } = useAuth();
+
     return (
         <div className="app">
             <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-                <Route path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+                <Route path="/" element={isAuthenticated ? <PrivateRoute><Dashboard /></PrivateRoute> : <Navigate to="/login" />} />
                 <Route path="/commissar" element={<PrivateRoute><Commissar /></PrivateRoute>} />
                 <Route path="/escort" element={<PrivateRoute><Escort /></PrivateRoute>} />
             </Routes>
