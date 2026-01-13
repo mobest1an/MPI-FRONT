4ca0721e635c3f50bc1f31ca4b35a6b086e6b5fb
Ivan Kustarev <ivan.kustarev@tune-it.ru>
2026-01-14 00:59:27 +0300

    deploy fixes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './styles/auth.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/~s311293">
             <AuthProvider>
                 <App />
             </AuthProvider>
